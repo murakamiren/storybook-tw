64a8ffeaec8c8563112eff23ae768f124b330939
renmurakami <autoeyy@gmail.com>
2022-04-24 18:05:45 +0900

    jest mock

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,11 @@ module.exports = {
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest"
   },
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "./__mocks__/fileMock.js",
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js"
+  },
   preset: "ts-jest",
   testEnvironment: "jsdom",
   roots: ["./src/tests"],
